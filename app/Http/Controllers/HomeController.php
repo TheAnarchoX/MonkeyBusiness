f452721ca37741ab6c36ee07462209fd45d9ddb5
C0ns7ruc7 <00093215@student.rocleiden.nl>
2018-05-22 09:54:27 +0200

    some basic stuff
    1% out of 1% todo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-        echo "NEE";
+        echo "JA";
     }
 }
---
+++ resources/views/Main.blade.php
@@ -0,0 +1,11 @@
+{{--/**--}}
+ {{--* Created by PhpStorm.--}}
+ {{--* User: Vabese--}}
+ {{--* Date: 22-5-2018--}}
+ {{--* Time: 9:46--}}
+ {{--*/--}}
+@extends ('Layout.Master')
+
+@section('content')
+
+@endsection
