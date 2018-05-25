93713723b95f2efe508d2fdd7008d36d3d472811
Michael Rotteveel <00124415@student.rocleiden.nl>
2018-05-25 13:25:11 +0200

    Added dummy content to page

--- resources/views/Main.blade.php
+++ resources/views/Main.blade.php
@@ -1,5 +1,40 @@
 @extends ('layouts.Master')
 
 @section('content')
-
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, consectetur cumque dolore eos illo magnam modi
+        mollitia, non, obcaecati perspiciatis provident quae quas recusandae tenetur voluptas? A eveniet reprehenderit
+        totam.
+    </div>
+    <div>Alias animi consequatur culpa eaque facilis iusto, neque placeat possimus provident rem sequi sunt. A aut
+        cumque distinctio ducimus eveniet fugit iste libero magni nihil perspiciatis quidem quod, sed voluptatum.
+    </div>
+    <div>Animi at, beatae consequuntur cupiditate delectus dolore expedita explicabo, laudantium maxime, neque omnis
+        placeat quae quaerat qui quos rem reprehenderit sequi sit totam velit. Distinctio eum eveniet id omnis
+        perferendis?
+    </div>
+    <div>A aspernatur, at autem commodi cum cumque dicta dolorem earum eligendi excepturi fugiat labore, laborum magni
+        maxime nemo nesciunt nostrum omnis praesentium quis ratione repellat reprehenderit sint tempora ullam vitae!
+    </div>
+    <div>Ab accusamus architecto at cumque debitis delectus distinctio dolor dolorem ea eaque eos est et iusto modi
+        molestias nesciunt nostrum officiis quod quos rem repellendus reprehenderit, tempore ut veritatis voluptas?
+    </div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae consectetur corporis dignissimos dolores eos
+        fugit inventore ipsa laboriosam natus, neque nesciunt nihil omnis praesentium reiciendis sit ullam voluptatem
+        voluptatibus!
+    </div>
+    <div>Adipisci autem consequatur hic nesciunt tempora, veniam! Accusantium dolorem doloremque esse fugiat, iusto
+        magni molestias, nulla omnis quam quasi similique ullam vero. Ab aliquam deleniti deserunt fuga minima
+        perferendis rem!
+    </div>
+    <div>Consequuntur ipsum rerum sequi. Cum cumque dolor est et ex fuga ipsum molestias nesciunt obcaecati
+        perspiciatis, porro quam quas quasi ratione similique! Aliquid, cum enim mollitia nostrum quibusdam quo saepe.
+    </div>
+    <div>Ab aliquam atque blanditiis consequuntur explicabo perferendis porro, suscipit veritatis. Cupiditate dolorem
+        est quasi quibusdam? Magnam, sed vitae. Aliquid architecto beatae dolorum error in mollitia officiis, quibusdam
+        ratione sunt velit.
+    </div>
+    <div>Accusamus accusantium, assumenda doloremque dolorum hic iusto labore magnam nam natus officia sunt velit!
+        Dolorem ea esse iusto. Aliquid aperiam atque dolores doloribus incidunt laborum pariatur sapiente suscipit
+        voluptatem voluptates!
+    </div>
 @endsection
