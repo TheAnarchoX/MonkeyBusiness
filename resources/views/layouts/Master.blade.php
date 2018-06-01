1a3eba5fac9a2723f797af64230e62dfd8ea09ec
Michael Rotteveel <00124415@student.rocleiden.nl>
2018-06-01 09:39:53 +0200

    Header fixings also overzichterlijk

--- resources/views/layouts/Master.blade.php
+++ resources/views/layouts/Master.blade.php
@@ -23,26 +23,7 @@
 <body>
 <div class="container-fluid">
     <div class="row">
-        <header class="img-wrapper">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="d-flex justify-content-start navbar-nav">
-                        <li class="nav-link"><a href="{{'public.landing'}}" class="nav-item">Home</a></li>
-                        <li class="nav-link"><a href="{{route('public.activiteiten.index')}}" class="nav-item">Activiteiten</a></li>
-                        <li class="nav-link"><a href="{{route('public.partners.index')}}" class="nav-item">Partners</a></li>
-                    </ul>
-                    <a class="d-flex justify-content-center"><img src="{{asset('images/logo.png')}}"></a>
-                    <ul class="d-flex justify-content-end navbar-nav">
-                        <li class="nav-link"><a href="{{route('public.nieuws.index')}}" class="nav-item">Nieuws</a></li>
-                        <li class="nav-link"><a href="{{route('public.fotos.index')}}" class="nav-item">Foto's</a></li>
-                        <li class="nav-link"><a href="{{route('public.berichten.create')}}" class="nav-item">Contact</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        @include('layouts.Nav')
         <main class="main-content">
             @yield('content')
         </main>
