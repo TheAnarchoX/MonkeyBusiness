089260ff41b276dbf5d4cd7aff7357d5aba745a8
Michael Rotteveel <00124415@student.rocleiden.nl>
2018-06-19 10:40:54 +0200

    [Footer] added footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,29 @@
             @yield('content')
         </main>
     </div>
+    <div class="row">
+        <div class="footer-transition"></div>
+        <div class="footer fixed-bottom bg-green overlay w-100 position-relative mb-0">
+            <footer class="d-flex">
+                <div class="col-md-3">
+                    <h3>TEST HEADER</h3>
+
+                </div>
+                <div class="col-md-3">
+                    <h3>TEST HEADER</h3>
+
+                </div>
+                <div class="col-md-3">
+                    <h3>TEST HEADER</h3>
+
+                </div>
+                <div class="col-md-3">
+                    <h3>TEST HEADER</h3>
+
+                </div>
+            </footer>
+        </div>
+    </div>
 </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
