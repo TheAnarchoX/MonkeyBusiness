deccbdb9f5a9b1b5b3a05580bac4a2a3b22df90e
Michael Rotteveel <00124415@student.rocleiden.nl>
2018-06-26 10:18:12 +0200

    [Activity] Added data and paginatation
    php artisan vendor:publish --tag=laravel-pagination

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use Illuminate\Http\Request;
 
 class ActivityController extends Controller
@@ -13,7 +14,9 @@ class ActivityController extends Controller
      */
     public function index()
     {
-        return view('frontend.activiteiten.index');
+
+    	$activities = Activity::paginate(12);
+        return view('frontend.activiteiten.index', compact('activities'));
     }
 
     /**
--- resources/views/frontend/activiteiten/index.blade.php
+++ resources/views/frontend/activiteiten/index.blade.php
@@ -5,5 +5,13 @@
 @endsection
 
 @section('content')
-
+    <div class="row">
+        @foreach($activities as $activity)
+            <div class="col-md-6">
+                <a href="{{route('public.activiteiten.show', $activity->slug)}}"><h2>{{$activity->title}}</h2></a>
+                <p>{{$activity->description}}</p>
+            </div>
+        @endforeach
+            {{$activities->links('vendor.pagination.bootstrap-4')}}
+    </div>
 @endsection
